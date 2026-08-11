2e442eb733207e1904b02c125a20a9720b1fcca9
Himesh Purohit <himeshpuro@gmail.com>
2026-08-11 15:10:00 +0530

    Implement custom CurrentUser decorator

---
+++ src/auth/decorators/current-user.decorator.ts
@@ -0,0 +1,10 @@
+import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+import type { JwtPayload } from '../auth.service';
+
+export const CurrentUser = createParamDecorator(
+  (_data: unknown, ctx: ExecutionContext): JwtPayload => {
+    const request = ctx.switchToHttp().getRequest();
+    return request.user;
+  },
+);
+
